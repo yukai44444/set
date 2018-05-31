a44eca33a513ac0821c108554bcb2f19fa823052
余秉烨 <yukai44444@gmail.com>
2018-05-31 14:37:57 +0800

    新macbook

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-ln -s "`pwd`/vim" "$HOME/.vim"
-ln -s "`pwd`/vimrc" "$HOME/.vimrc"
 ln -s "`pwd`/zshrc" "$HOME/.zshrc"
 ln -s "`pwd`/gitconfig" "$HOME/.gitconfig"
 ln -s "`pwd`/gitignore" "$HOME/.gitignore"
